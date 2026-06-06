60f6f5c8f0c01dafd63668599344bcde1aaa7ac7
Ana-git-22 <anajulia.cunhapereira@gmail.com>
2026-06-06 17:04:16 -0300

    Initial Commit

---
+++ TaskFlit/TaskFlitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlitApp.swift
+//  TaskFlit
+//
+//  Created by Ana Julia da Cunha Pereira on 06/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskFlitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
